0a0089f99161733bcae7deeb18d2bad044d86be1
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-12-12 23:30:02 +0100

    added the Css Modules for the Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,16 @@
+// Importo i CSS Modules delle Card
+import style from "../card.module.css";
 /* Funzione che La Card */
 function Card({ cities }) {
-
+    // Mappo props cities per popolare le card
     const cityArray = cities.map((city) => {
         return (
-            <div key={city.id} className="card cards col-6 col-sm-6 col-md-3 col-lg-3 mb-3 mt-3 p-0 column-gap-3" id="cards">
-                <img src={`${city.image}`} className="card-img-top img-custom" alt={city.title} />
+            <div key={city.id} className={`${style.cards} card col-6 col-sm-6 col-md-3 col-lg-3 mb-3 mt-3 p-0 column-gap-3`}>
+                <img src={`${city.image}`} className={`card-img-top ${style["img-custom"]}`} alt={city.title} />
                 <div className="card-body">
                     <h5 className="card-title">{city.title}</h5>
                     <p className="card-text">{city.content}</p>
-                    <a href="#" className="btn btn-primary btn-custom">Leggi di più</a>
+                    <a href="#" className={`btn btn-primary ${style.btncustom}`}>Leggi di più</a>
                 </div>
             </div>
         );
