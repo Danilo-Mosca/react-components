5d1272004590ef62483fcd3b03249bb21e4bd5d8
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-12-12 20:37:03 +0100

    Created object cities as a database. Create cards with properties: image, title and content for each database element

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,22 @@ import { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Card from './components/Card';
+// Importo l'oggetto cities
+import cities from './data/cities';
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const city = [...cities];
+  
   //parte html da ritornare
   return (
     <>
       <Header />
-
-    
         <main className="container">
           <div className="row">
-              <Card />
-              <Card />
-              <Card />
-              <Card /> 
-              <Card />
+          <Card cities={city} />
           </div>
         </main>
-
       <Footer />
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,18 @@
 /* Funzione che La Card */
-function Card() {
-    const title = "Il Mio blog";
-    return (
-        <div className="card cards col-6 col-sm-6 col-md-3 col-lg-3 mb-3 mt-3 p-0 column-gap-3" id="cards">
-            <img src="https://placehold.co/600x400" className="card-img-top img-custom" alt="immagine 600x400px" />
-            <div className="card-body">
-                <h5 className="card-title">Titolo del post</h5>
-                <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus culpa excepturi ullam quas ab autem rerum quisquam debitis atque accusamus rem, temporibus blanditiis inventore, fugit odit?</p>
-                <a href="#" className="btn btn-primary btn-custom">Leggi di più</a>
+function Card({ cities }) {
+
+    const cityArray = cities.map((city) => {
+        return (
+            <div key={city.id} className="card cards col-6 col-sm-6 col-md-3 col-lg-3 mb-3 mt-3 p-0 column-gap-3" id="cards">
+                <img src={`${city.image}`} className="card-img-top img-custom" alt={city.title} />
+                <div className="card-body">
+                    <h5 className="card-title">{city.title}</h5>
+                    <p className="card-text">{city.content}</p>
+                    <a href="#" className="btn btn-primary btn-custom">Leggi di più</a>
+                </div>
             </div>
-        </div>
-    );
+        );
+    });
+    return cityArray;
 }
 export default Card;
